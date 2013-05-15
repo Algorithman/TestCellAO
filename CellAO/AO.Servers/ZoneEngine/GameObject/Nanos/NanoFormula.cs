5b45e8fe0d527c5782155ce080de189ec0802956
Algorithman <algorithman.chris@googlemail.com>
2013-05-15 21:50:53 +0200

    Added Nanos

---
+++ CellAO/AO.Servers/ZoneEngine/GameObject/Nanos/NanoFormula.cs
@@ -0,0 +1,289 @@
+﻿#region License
+
+// Copyright (c) 2005-2013, CellAO Team
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+// 
+//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//     * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+namespace ZoneEngine.GameObject.Nanos
+{
+    #region Usings ...
+
+    using System;
+    using System.Collections.Generic;
+
+    using ZoneEngine.GameObject.Items;
+
+    #endregion
+
+    /// <summary>
+    /// NanoFormula serializable class
+    /// </summary>
+    [Serializable]
+    public class NanoFormula
+    {
+        /// <summary>
+        /// Item Flags
+        /// </summary>
+        public int flags;
+
+        /// <summary>
+        /// Nano ID
+        /// </summary>
+        public int ID;
+
+        /// <summary>
+        /// NCUCost
+        /// </summary>
+        public int NCUCost;
+
+        /// <summary>
+        /// Type of instanced item
+        /// </summary>
+        public int Type;
+
+        /// <summary>
+        /// Instance of instanced item
+        /// </summary>
+        public int Instance;
+
+        /// <summary>
+        /// Item type
+        /// </summary>
+        public int ItemType;
+
+        /// <summary>
+        /// Item attributes
+        /// </summary>
+        public List<AOItemAttribute> Stats = new List<AOItemAttribute>();
+
+        /// <summary>
+        /// List of Attack attributes
+        /// </summary>
+        public List<AOItemAttribute> Attack = new List<AOItemAttribute>();
+
+        /// <summary>
+        /// List of defensive attributes
+        /// </summary>
+        public List<AOItemAttribute> Defend = new List<AOItemAttribute>();
+
+        /// <summary>
+        /// List of Item events
+        /// </summary>
+        public List<AOEvents> Events = new List<AOEvents>();
+
+        /// <summary>
+        /// List of Item Actions (requirement checks)
+        /// </summary>
+        public List<AOActions> Actions = new List<AOActions>();
+
+        /// <summary>
+        /// </summary>
+        /// Methods to do:
+        /// Read Item
+        /// Write Item
+        /// Return Dynel Item (placing on the ground)
+        /// <returns>
+        /// </returns>
+        public NanoFormula ShallowCopy()
+        {
+            NanoFormula nanoFormula = new NanoFormula();
+            nanoFormula.ID = this.ID;
+
+            foreach (AOItemAttribute nanoFormulaAttribute in this.Attack)
+            {
+                AOItemAttribute z = new AOItemAttribute();
+                z.Stat = nanoFormulaAttribute.Stat;
+                z.Value = nanoFormulaAttribute.Value;
+                nanoFormula.Attack.Add(z);
+            }
+
+            foreach (AOItemAttribute nanoFormulaAttribute in this.Defend)
+            {
+                AOItemAttribute z = new AOItemAttribute();
+                z.Stat = nanoFormulaAttribute.Stat;
+                z.Value = nanoFormulaAttribute.Value;
+                nanoFormula.Defend.Add(z);
+            }
+
+            foreach (AOItemAttribute nanoFormulaAttribute in this.Stats)
+            {
+                AOItemAttribute z = new AOItemAttribute();
+                z.Stat = nanoFormulaAttribute.Stat;
+                z.Value = nanoFormulaAttribute.Value;
+                nanoFormula.Stats.Add(z);
+            }
+
+            foreach (AOEvents aoEvents in this.Events)
+            {
+                AOEvents newEvent = new AOEvents();
+                foreach (AOFunctions aoFunctions in aoEvents.Functions)
+                {
+                    AOFunctions newAOFunctions = new AOFunctions();
+                    foreach (AORequirements aor in aoFunctions.Requirements)
+                    {
+                        AORequirements newRequirement = new AORequirements();
+                        newRequirement.ChildOperator = aor.ChildOperator;
+                        newRequirement.Operator = aor.Operator;
+                        newRequirement.Statnumber = aor.Statnumber;
+                        newRequirement.Target = aor.Target;
+                        newRequirement.Value = aor.Value;
+                        newAOFunctions.Requirements.Add(newRequirement);
+                    }
+
+                    foreach (object argument in aoFunctions.Arguments.Values)
+                    {
+                        if (argument.GetType() == typeof(string))
+                        {
+                            string z = (string)argument;
+                            newAOFunctions.Arguments.Values.Add(z);
+                        }
+
+                        if (argument.GetType() == typeof(int))
+                        {
+                            int i = (int)argument;
+                            newAOFunctions.Arguments.Values.Add(i);
+                        }
+
+                        if (argument.GetType() == typeof(Single))
+                        {
+                            float s = (Single)argument;
+                            newAOFunctions.Arguments.Values.Add(s);
+                        }
+                    }
+
+                    newAOFunctions.dolocalstats = aoFunctions.dolocalstats;
+                    newAOFunctions.FunctionType = aoFunctions.FunctionType;
+                    newAOFunctions.Target = aoFunctions.Target;
+                    newAOFunctions.TickCount = aoFunctions.TickCount;
+                    newAOFunctions.TickInterval = aoFunctions.TickInterval;
+                    newEvent.Functions.Add(newAOFunctions);
+                }
+
+                newEvent.EventType = aoEvents.EventType;
+                nanoFormula.Events.Add(newEvent);
+            }
+
+            nanoFormula.flags = this.flags;
+            nanoFormula.Instance = this.Instance;
+            nanoFormula.ItemType = this.ItemType;
+            nanoFormula.NCUCost = this.NCUCost;
+
+            return nanoFormula;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public bool isInstanced()
+        {
+            if ((this.Type == 0) && (this.Instance == 0))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        #region GetWeaponStyle
+
+        /// <summary>
+        /// Get WeaponStyle (Stat 274)
+        /// </summary>
+        /// <returns>Value of Stat 274 or 0</returns>
+        public int GetWeaponStyle()
+        {
+            foreach (AOItemAttribute at in this.Stats)
+            {
+                if (at.Stat != 274)
+                {
+                    continue;
+                }
+
+                return at.Value;
+            }
+
+            // Odd, no WeaponWieldFlags found...
+            return 0;
+        }
+
+        #endregion
+
+        #region Get Override Texture
+
+        /// <summary>
+        /// Get override texture number
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public int GetOverrideTexture()
+        {
+            foreach (AOItemAttribute attr in this.Stats)
+            {
+                if (attr.Stat != 336)
+                {
+                    continue;
+                }
+
+                return attr.Value;
+            }
+
+            // No Override OH NOES!
+            return 0;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Get item attribute
+        /// </summary>
+        /// <param name="number">
+        /// number of attribute
+        /// </param>
+        /// <returns>
+        /// Value of item attribute
+        /// </returns>
+        public int getItemAttribute(int number)
+        {
+            int c;
+            for (c = 0; c < this.Stats.Count; c++)
+            {
+                if (number == this.Stats[c].Stat)
+                {
+                    return this.Stats[c].Value;
+                }
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Nano strain
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public int NanoStrain()
+        {
+            return this.getItemAttribute(75);
+        }
+    }
+}
---
+++ CellAO/AO.Servers/ZoneEngine/GameObject/Nanos/NanoHandler.cs
@@ -0,0 +1,131 @@
+﻿
+#region Usings...
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using ComponentAce.Compression.Libs.zlib;
+using MsgPack.Serialization;
+#endregion
+
+namespace ZoneEngine.GameObject.Nanos
+{
+    /// <summary>
+    /// Item handler class
+    /// </summary>
+    public class NanoHandler
+    {
+
+        /// <summary>
+        /// Cache of all item templates
+        /// </summary>
+        public static List<NanoFormula> NanoList = new List<NanoFormula>();
+
+        /// <summary>
+        /// Cache all item templates
+        /// </summary>
+        /// <returns>number of cached items</returns>
+        public static int CacheAllNanos()
+        {
+            DateTime _now = DateTime.Now;
+            NanoList = new List<NanoFormula>();
+            Stream sf = new FileStream("nanos.dat", FileMode.Open);
+            MemoryStream ms = new MemoryStream();
+
+            ZOutputStream sm = new ZOutputStream(ms);
+            CopyStream(sf, sm);
+
+            ms.Seek(0, SeekOrigin.Begin);
+
+            byte[] buffer = new byte[4];
+            ms.Read(buffer, 0, 4);
+            int packaged = BitConverter.ToInt32(buffer, 0);
+
+            BinaryReader br = new BinaryReader(ms);
+            var bf = MessagePackSerializer.Create<List<NanoFormula>>();
+
+            while (true)
+            {
+                List<NanoFormula> templist = bf.Unpack(ms);
+                NanoList.AddRange(templist);
+                if (templist.Count != packaged)
+                {
+                    break;
+                }
+                Console.Write("Loaded {0} Nanos in {1}\r",
+                              new object[] { NanoList.Count, new DateTime((DateTime.Now - _now).Ticks).ToString("mm:ss.ff") });
+            }
+            GC.Collect();
+            return NanoList.Count;
+        }
+
+        /// <summary>
+        /// Cache all item templates
+        /// </summary>
+        /// <returns>number of cached items</returns>
+        public static int CacheAllNanos(string fname)
+        {
+            DateTime _now = DateTime.Now;
+            NanoList = new List<NanoFormula>();
+            Stream sf = new FileStream(fname, FileMode.Open);
+            MemoryStream ms = new MemoryStream();
+
+            ZOutputStream sm = new ZOutputStream(ms);
+            CopyStream(sf, sm);
+
+            ms.Seek(0, SeekOrigin.Begin);
+            BinaryReader br = new BinaryReader(ms);
+            var bf = MessagePackSerializer.Create<List<NanoFormula>>();
+
+
+            byte[] buffer = new byte[4];
+            ms.Read(buffer, 0, 4);
+            int packaged = BitConverter.ToInt32(buffer, 0);
+
+            while (true)
+            {
+                List<NanoFormula> templist = (List<NanoFormula>)bf.Unpack(ms);
+                NanoList.AddRange(templist);
+                if (templist.Count != packaged)
+                {
+                    break;
+                }
+                Console.Write("Loaded {0} nanos in {1}\r",
+                              new object[] { NanoList.Count, new DateTime((DateTime.Now - _now).Ticks).ToString("mm:ss.ff") });
+            }
+            GC.Collect();
+            return NanoList.Count;
+        }
+
+
+        private static void CopyStream(Stream input, Stream output)
+        {
+            byte[] buffer = new byte[2097152];
+            int len;
+            while ((len = input.Read(buffer, 0, 2097152)) > 0)
+            {
+                output.Write(buffer, 0, len);
+                Console.Write("\rDeflating " + Convert.ToInt32(Math.Floor((double)input.Position / input.Length * 100.0)) +
+                              "%");
+            }
+            output.Flush();
+            Console.Write("\r                                             \r");
+        }
+
+
+        /// <summary>
+        /// Returns a nano object
+        /// </summary>
+        /// <param name="id">ID of the nano</param>
+        /// <returns>Nano</returns>
+        public static NanoFormula GetNano(int id)
+        {
+            foreach (NanoFormula nanoFormula in NanoList)
+            {
+                if (nanoFormula.ID == id)
+                    return nanoFormula;
+            }
+            return null;
+        }
+    }
+}
