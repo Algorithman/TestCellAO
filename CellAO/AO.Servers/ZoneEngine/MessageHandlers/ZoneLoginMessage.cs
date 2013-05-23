9419b105ed0357e161aa6b2d3859d01dd9fb6970
Algorithman <algorithman.chris@googlemail.com>
2013-05-23 20:57:24 +0200

    Login without head

--- CellAO/AO.Servers/ZoneEngine/MessageHandlers/ZoneLoginMessage.cs
+++ CellAO/AO.Servers/ZoneEngine/MessageHandlers/ZoneLoginMessage.cs
@@ -29,6 +29,7 @@ namespace ZoneEngine.MessageHandlers
             var zoneLoginMessage = (ZoneLoginMessage)message.Body;
             client.CreateCharacter(zoneLoginMessage.CharacterId);
             client.Character.Playfield = client.Playfield;
+            client.Character.Stats.ReadStatsfromSql();
 
             Thread.Sleep(1000);
             ClientConnected tmpClientConnected = new ClientConnected();
